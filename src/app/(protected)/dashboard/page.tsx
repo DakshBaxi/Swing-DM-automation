943a098547cf7bef41a4a45b4e34dd8692e2b92b
DakshBaxi <baxidaksh2004@gmail.com>
2025-03-09 12:50:36 +0530

    .env file removed

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-const Page = (props: Props) => {
+const Page = async (props: Props) => {
 
     // WIP:server action to onboard the user
+    const user = await onBoardUser()
     // WIP:200 ||201
   return (
     <div>Dashboard</div>
